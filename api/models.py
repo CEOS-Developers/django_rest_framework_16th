2a18057b763d97f48a3bbbf4e2b4a381d19a9cc6
squirMM <jsm6616@naver.com>
2022-09-30 02:26:22 +0900

    :rocket: Basemodel 생성

--- api/models.py
+++ api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+class BaseModel(models.Model):
+    is_deleted = models.BooleanField(default=False)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    deleted_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+    def delete(self, using=None, keep_parents=False):
+        self.is_deleted = True
+        self.deleted_at = datetime.now()
+        self.save()
