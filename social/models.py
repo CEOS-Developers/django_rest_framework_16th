129661c0e620289ec1dcb6ff827427af81f6a9c6
aqswa <69039161+aqswa@users.noreply.github.com>
2022-10-01 18:33:23 +0900

    social model

--- social/models.py
+++ social/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Following(models.Model):
+    profile = models.ForeignKey('Profile', on_delete=models.CASCADE, related_name='following')
