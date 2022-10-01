84494f6ed1ef99e434096d010369359dfb4c8fa1
aqswa <69039161+aqswa@users.noreply.github.com>
2022-10-01 19:25:24 +0900

    diary app model

--- social/models.py
+++ social/models.py
@@ -1,8 +1,15 @@
 from django.db import models
-from account.models import Profile
+from account.models import Profile, User
+from todo.models import Todo
 
 # Create your models here.
 
 
 class Following(models.Model):
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='following')
+
+
+class TodoCheer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    todo = models.ForeignKey(Todo, on_delete=models.CASCADE)
+    imoji = models.ImageField()
