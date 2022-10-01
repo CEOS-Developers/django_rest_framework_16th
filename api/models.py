ff869ffdef9087b25fa4002731d7d00825fee977
chaeshee0908 <chaeshee0908@naver.com>
2022-10-01 22:41:45 +0900

    날짜 형식 변경

--- api/models.py
+++ api/models.py
@@ -9,23 +9,31 @@ class User(models.Model):
     intro_text = models.TextField(null=True)
     createdAt = models.DateTimeField()
 
+    def __str__(self):
+        return self.name
+
 
 class Todo(models.Model):
     user = models.ForeignKey('User', on_delete=models.CASCADE, verbose_name='user info')
     contents = models.TextField()
-    date = models.DateTimeField()
+    date = models.DateField(input_formats=['%Y/%m/%d'])
     is_checked = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.contents
 
 class Diary(models.Model):
     user = models.ForeignKey('User', on_delete=models.CASCADE, verbose_name='user info')
     emoji = models.CharField(max_length=15)
     mood_temperature = models.IntegerField(default=25)
-    date = models.DateTimeField()
+    date = models.DateField(input_formats=['%Y/%m/%d'])
     contents = models.TextField()
     background_color = models.CharField(max_length=15)
     private = models.BooleanField(default=False)
     image = models.ImageField(null=True)
+
+    def __str__(self):
+        return '{} 일기'.format(self.user.name)
 
 
 class Likes(models.Model):
@@ -34,9 +42,15 @@ class Likes(models.Model):
     todo = models.ManyToManyField(Todo)
     diary = models.ManyToManyField(Diary)
 
+    def __str__(self):
+        return '{}가 {}의 글을 좋아합니다.'.format(self.click_user.name, self.todo.user.name)
+
 
 class Follows(models.Model):
     # 팔로우를 건 유저
     click_user = models.ManyToManyField(User, related_name='click_user')
-    # 팔로우 타켓
+    # 팔로우 타겟
     target = models.ManyToManyField(User, related_name='target_user')
+
+    def __str__(self):
+        return '{}가 {}를 팔로우합니다.'.format(self.click_user.name, self.target.name)
