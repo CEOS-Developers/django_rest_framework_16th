6463b72192f7e79c2a7680f9719126ee019020e2
aqswa <69039161+aqswa@users.noreply.github.com>
2022-10-01 20:45:01 +0900

    Fix: ㅁaccount app model

--- account/models.py
+++ account/models.py
@@ -20,6 +20,8 @@ class Profile(models.Model):
     social_id = models.CharField(max_length=100, blank=True, unique=True, validators=[MinLengthValidator(4)])
     image = models.ImageField(null=True, blank=True)
     following = models.ManyToManyField('User', related_name='follower')
+    is_searchable = models.BooleanField(default=True)
+    is_displayed = models.BooleanField(default=True)
 
     def __str__(self):
         return "keep: " + self.nickname
