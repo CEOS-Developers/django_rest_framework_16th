ed734f7fd5d360f93cbe44c5f5e22687641f332b
geniee44 <68368633+geniee44@users.noreply.github.com>
2022-10-01 21:19:05 +0900

    Fix default of DateField

--- api/models.py
+++ api/models.py
@@ -2,6 +2,7 @@ import datetime
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 
 class Profile(models.Model):
@@ -15,16 +16,16 @@ class Profile(models.Model):
 
 # User를 onetoone 방식으로 하면 나머지 클래스에서 foreignkey로 user id를 쓰고 싶을 때 어떻게 해야 하는지 궁금함
 class Follower(models.Model):
-    profile = models.ForeignKey(Profile, on_delete=models.CASCADE())
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
 
 
 class Following(models.Model):
-    profile = models.ForeignKey(Profile, on_delete=models.CASCADE())
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
 
 
 class Category(models.Model):
     category_id = models.AutoField(primary_key=True)
-    profile = models.ForeignKey(Profile, on_delete=models.CASCADE())
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
     title = models.TextField(max_length=50)
     color = models.IntegerField(default=0)
     is_public = models.BooleanField(default=False)
@@ -33,20 +34,20 @@ class Category(models.Model):
 
 class Todo(models.Model):
     todo_id = models.AutoField(primary_key=True)
-    profile = models.ForeignKey(Profile, on_delete=models.CASCADE())
-    category = models.ForeignKey(Category, on_delete=models.CASCADE())  # 여기를 CASCADE로 설정하는 것이 맞는지 모르겠다.
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)  # 여기를 CASCADE로 설정하는 것이 맞는지 모르겠다.
     is_success = models.BooleanField(default=False)
     is_valid = models.BooleanField(default=False)
     is_deleted = models.BooleanField(default=False)
-    deadline = models.DateField(default=datetime.date.today())
+    deadline = models.DateField(default=timezone.localtime())
     alarm = models.DateTimeField(null=True)
     content = models.CharField(max_length=200)
 
 
 class Diary(models.Model):
     diary_id = models.AutoField(primary_key=True)
-    profile = models.ForeignKey(Profile, on_delete=models.CASCADE())
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
     content = models.CharField(max_length=400)
     emoji = models.CharField(max_length=100)
     public = models.IntegerField(default=0)
-    deadline = models.DateField(default=datetime.date.today())
+    deadline = models.DateField(default=timezone.localtime())
