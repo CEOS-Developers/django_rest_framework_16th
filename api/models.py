cb90d83f9ab7dbd12e35f87f8be36b961194ac8c
yjoonjang <yjoonjang34@gmail.com>
2022-10-01 22:02:10 +0900

    feat: Todo와 Diary 모델 생성 및 관련 README 업데이트

--- api/models.py
+++ api/models.py
@@ -1,12 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_intro = models.TextField(blank=True, null=True)  # 길이 제한이 없는 문자열
-    profile_image = models.ImageField(blank=True, null=True)
-
-
 class User(models.Model):
     id = models.BigIntegerField()   # key
     username = models.TextField(max_length=30)
@@ -17,4 +11,25 @@ class User(models.Model):
     is_shown = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)  # 해당 레코드 생성시 현재 시간 자동저장
     updated_at = models.DateTimeField(auto_now=True)  # 해당 레코드 갱신시 현재 시간 자동저장
+    deleted_at = models.DateTimeField()
+
+class Todo(models.Model):
+    id = models.BigIntegerField()
+    user_id = models.BigIntegerField()
+    is_done = models.BooleanField()
+    is_stored = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField()
+
+class Diary(models.Model):
+    id = models.BigIntegerField()
+    user_id = models.BigIntegerField()
+    content = models.TextField()
+    bg_color = models.TextField()
+    feeling_temp = models.IntegerField()
+    emoji = models.CharField()
+    is_secret = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     deleted_at = models.DateTimeField()
