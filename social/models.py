596b209451e3107cca8163c53d6cb735af931d1e
aqswa <69039161+aqswa@users.noreply.github.com>
2022-10-01 19:27:08 +0900

    Add: social app model

--- social/models.py
+++ social/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from account.models import Profile, User
 from todo.models import Todo
+from diary.models import Diary
+
 
 # Create your models here.
 
@@ -12,4 +14,10 @@ class Following(models.Model):
 class TodoCheer(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     todo = models.ForeignKey(Todo, on_delete=models.CASCADE)
+    imoji = models.ImageField()
+
+
+class DiaryCheer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    diary = models.ForeignKey(Diary, on_delete=models.CASCADE)
     imoji = models.ImageField()
