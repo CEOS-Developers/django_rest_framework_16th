8895859e20dc60e8096c3bacf1d83a3d92c50af2
wjdtkdgns <wjdtkdgns329@naver.com>
2022-10-01 19:22:24 +0900

    🚀 feat: 모델 이름 수정, edit 리드미

--- README.md
+++ README.md
@@ -9,14 +9,23 @@
 3. 팔로워와 자신의 일정을 공유하고, 일기와 할 일에 관해 좋아요(이모지)를 남길 수 있음
 
 ## ERD
-![erd](https://user-images.githubusercontent.com/67852689/193394096-e3eb8f21-4188-4e33-9e26-6d47784dd5ba.jpeg)
+![erd](https://user-images.githubusercontent.com/67852689/193404695-d5c88992-af38-4b0d-b600-35cb35440d6e.jpeg)
 * User : Django의 유저모델과 OneToOne 확장 이용, 유저의 정보를 저장
-* Goal : 목표에 대한 정보를 저장, 1(User) : N(Goal), 1(Goal) : N(Todo)
-* Todo : 투두에 관한 정보를 저장, 1(Goal) : N(Todo)
-* TodoLike : 투두의 좋아요 정보를 저장, 1(Todo) : N(TodoLike), 1(User) : 1(TodoLike)
-* Diary : 일기에 관한 정보를 저장, 1(User) : N(Diary)
-* DiaryLike : 일기의 좋아요 정보를 저장, 1(Diary) : N(DiaryLike), 1(User) : 1(DiaryLike)
-* Follow : 팔로우 관련 정보를 저장, 1(User) : N(Following)
+* Goal : 목표에 대한 정보를 저장
+  * 1(User) : N(Goal) : 유저가 여러 개의 목표를 생성할 수 있다.
+  * 1(Goal) : N(Todo) : 한 목표에 여러 투두를 생성할 수 있다.
+* Todo : 투두에 관한 정보를 저장
+  * 1(Goal) : N(Todo)
+* TodoLike : 투두의 좋아요 정보를 저장
+  * 1(Todo) : N(TodoLike) : 한 투두에 많은 수의 좋아요를 가질 수 있다.
+  * 1(User) : 1(TodoLike) : 각 좋아요는 한 명의 유저와 연관되어 있으므로, 1대 1로 만들었다.
+* Diary : 일기에 관한 정보를 저장
+  * 1(User) : N(Diary) : 일기를 날짜별로 적을 수 있다. 그러므로 유저 한 명은 여러 개의 일기를 적을 수 있다.
+* DiaryLike : 일기의 좋아요 정보를 저장
+  * 1(Diary) : N(DiaryLike) : 일기도 투두와 마찬가지로 많은 수의 좋아요를 가질 수 있다.
+  * 1(User) : 1(DiaryLike) : Todo 좋아요와 이유는 동일하다
+* Follow : 팔로우 관련 정보를 저장
+  * 1(User) : N(Following) : 한 유저와 많은 수의 유저가 팔로우 관계를 맺을 수 있다.
 
 ## migration
 ```shell
@@ -26,3 +35,22 @@ python manage.py migrate api
 * makemigrations : 모델을 변경시킨 사실 또는 새로 생성한 모델들과 같은 변경사항을 migrations로 저장
 * migration : Django가 모델의 변경사항을 저장
 
+## ORM 사용해보기
+![image](https://user-images.githubusercontent.com/67852689/193402878-b9dd40d1-ad7b-4a91-9161-ef7e1d41a918.png)
+> 유저 모델 객체 3개
+
+![image](https://user-images.githubusercontent.com/67852689/193402877-da27e1b4-ca98-4622-8f2f-517e256dd3fd.png)
+> 유저 테이블 인스턴스를 외래키로 가지는 인스턴스를 생성 및 조회
+
+![image](https://user-images.githubusercontent.com/67852689/193402880-cea2dcf2-65ae-4b6b-8ac3-997a42fd9007.png)
+> filter를 이용해 조회
+
+## 회고
+1. django.db.utils.IntegrityError: (1048, "Column 'user_id' cannot be null")<br>
+Profile 인스턴스를 만들면 User가 자동으로 생성되는 줄 알았으나, 아니었다. 장고는 User를 자동적으로 생성해 주지 않기 때문에, Profile 인스턴스를 만들기 위해선,
+먼저 User 인스턴스를 만들어줘야 한다.
+2. MySQLdb.ProgrammingError: (1146, "Table 'todo.api_profile' doesn't exist")<br>
+결론부터 말하자면, 디비 문제였다. User 모델의 이름을 Profile로 바꾸면서 디비에 저장되어 있던 데이터와 코드에 적혀있는 모델과의 차이 때문에 발생했던 것 같다.
+디비를 초기화하고 migration 파일을 모두 삭제해서, 문제를 해결할 수 있었다.
+3. 어렵다<br>
+그래도 프론트보단 좋다ㅋ
--- api/models.py
+++ api/models.py
@@ -6,9 +6,16 @@ from django.contrib.auth.models import User
 
 # 장고에서 primary key 자동으로 만들어줌 (오토 인크레이징으로)
 
-class User(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    # OneToOne 방식 이용
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Profile(BaseModel):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)  # OneToOne 방식 이용
     user_name = models.CharField(max_length=20)
     email = models.EmailField()  # default max : 254
     profile_description = models.CharField(max_length=200, blank=True, null=True)
@@ -16,81 +23,67 @@ class User(models.Model):
     is_premium = models.BooleanField(default=False)
     can_search_byemail = models.BooleanField(default=False)
     can_show_todo = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.user_name
 
 
-class Goal(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='goal')
+class Goal(BaseModel):
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='goal')
     name = models.CharField(max_length=20)
     is_goal_private = models.BooleanField(default=True)
     color = models.CharField(max_length=20, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
 
 
-class Todo(models.Model):
-    goal_id = models.ForeignKey(Goal, on_delete=models.CASCADE, related_name='todo')
+class Todo(BaseModel):
+    goal = models.ForeignKey(Goal, on_delete=models.CASCADE, related_name='todo')
     todo_date = models.DateField()
     task = models.CharField(max_length=20)
     time = models.DateTimeField(blank=True, null=True)
     is_done = models.BooleanField(default=False)
     is_kept = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.task
 
 
-class Diary(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='diary')
+class Diary(BaseModel):
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='diary')
     date = models.DateField()
     emoji = models.CharField(max_length=20)
     text = models.CharField(max_length=200)
     temperature = models.SmallIntegerField(blank=True, null=True)
     color = models.CharField(max_length=20)
     is_diary_private = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return '{}의 일기'.format(self.user_id.user_name)
+        return '{}의 일기'.format(self.user.user_name)
 
 
-class Follow(models.Model):
-    follow_to_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='follow_to_id')
-    follow_from_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='follow_from_id')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+class Follow(BaseModel):
+    follow_to = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='follow_to_id')
+    follow_from = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='follow_from_id')
 
     def __str__(self):
-        return '{} to {}'.format(self.follow_from_id.user_name, self.follow_to_id.user_name)
+        return '{} to {}'.format(self.follow_from.user_name, self.follow_to.user_name)
 
 
-class DiaryLike(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='diary_like')
-    diary_id = models.ForeignKey(Diary, on_delete=models.CASCADE, related_name='diary_like')
+class DiaryLike(BaseModel):
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='diary_like')
+    diary = models.ForeignKey(Diary, on_delete=models.CASCADE, related_name='diary_like')
     emoji = models.CharField(max_length=20)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return '{}: {} 좋아요'.format(self.user_id.user_name, self.diary_id.date)
+        return '{}: {} 좋아요'.format(self.user.user_name, self.diary.date)
 
 
-class TodoLike(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='todo_like')
-    todo_id = models.ForeignKey(Todo, on_delete=models.CASCADE, related_name='todo_like')
+class TodoLike(BaseModel):
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='todo_like')
+    todo = models.ForeignKey(Todo, on_delete=models.CASCADE, related_name='todo_like')
     like_emoji = models.CharField(max_length=20)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return '{}: {} 좋아요'.format(self.user_id.user_name, self.todo_id.task)
+        return '{}: {} 좋아요'.format(self.user.user_name, self.todo.task)
