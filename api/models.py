45270a53280b9f28477a1622351943fc8df6576f
yjoonjang <yjoonjang34@gmail.com>
2022-10-01 21:23:31 +0900

    feat: User 모델 생성 및 README 업데이트

--- api/models.py
+++ api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_intro = models.TextField(blank=True, null=True)  # 길이 제한이 없는 문자열
+    profile_image = models.ImageField(blank=True, null=True)
+
+
+class User(models.Model):
+    id = models.BigIntegerField()   # key
+    username = models.TextField(max_length=30)
+    password = models.TextField(max_length=32)
+    email = models.TextField()
+    profile_image = models.imageField()
+    can_search_by_email = models.TextField()
+    is_shown = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)  # 해당 레코드 생성시 현재 시간 자동저장
+    updated_at = models.DateTimeField(auto_now=True)  # 해당 레코드 갱신시 현재 시간 자동저장
+    deleted_at = models.DateTimeField()
