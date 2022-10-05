a34618c6e17973d8b0ff8f9800bd4cd21471dbc4
Jeong-Hyeon-Lee <hyeon00@ewhain.net>
2022-10-05 16:36:56 +0900

    rename & modify models

--- api/models.py
+++ api/models.py
@@ -3,22 +3,24 @@ from django.contrib.auth.models import AbstractUser
 from django.utils.timezone import now
 # Create your models here.
 
+
 class User(AbstractUser):
     username = None
     email = models.EmailField(max_length=255, unique=True)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
-class Friends(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    subscription = models.EmailField(max_length=20) #팔로우하는 친구들의 email
+
+class Follower(models.Model):
+    follower = models.ForeignKey(User, related_name='follower', on_delete=models.CASCADE)
+    following = models.ForeignKey(User, related_name='following', on_delete=models.CASCADE)
 
 class Category(models.Model):
     category_name = models.CharField(max_length=50, null=False)
-    user = models.ForeignKey(
-        User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     def __str__(self):
         return self.category_name
+
 
 class Todo(models.Model):
     DISCLOSURE_CHOICES = {
@@ -35,7 +37,8 @@ class Todo(models.Model):
     def __str__(self):
         return self.todo_name
 
-class Communication(models.Model):
+
+class Comment(models.Model):
     todo = models.ForeignKey(Todo, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     emoji = models.CharField(max_length=1)
