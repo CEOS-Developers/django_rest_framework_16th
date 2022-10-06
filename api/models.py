0a0b687948e183f68240c4818df53f7ae1b6be0d
chaeshee0908 <chaeshee0908@naver.com>
2022-10-06 16:01:49 +0900

    BaseModel 적용 및 필드 옵션 변경

--- api/models.py
+++ api/models.py
@@ -7,51 +7,51 @@ class BaseModel(models.Model):
     class Meta:
         abstract = True
 
-class User(models.Model):
+class User(BaseModel):
     name = models.CharField(max_length=10)
-    email = models.CharField(max_length=25, unique=True)
+    email = models.EmailField()
     web_id = models.CharField(max_length=15, null=True)
     web_pw = models.CharField(max_length=25)
-    intro_text = models.TextField(null=True)
+    intro_text = models.TextField(null=True, blank=True, max_length=100)
 
     def __str__(self):
         return self.name
 
 
-class Todo(models.Model):
-    user = models.ForeignKey('User', on_delete=models.CASCADE, verbose_name='user info')
-    contents = models.TextField()
+class Todo(BaseModel):
+    user = models.ForeignKey('User', on_delete=models.CASCADE, related_name='todo')
+    contents = models.TextField(max_length=30)
     date = models.DateField()
     is_checked = models.BooleanField(default=False)
 
     def __str__(self):
         return self.contents
 
-class Diary(models.Model):
-    user = models.ForeignKey('User', on_delete=models.CASCADE, verbose_name='user info')
+class Diary(BaseModel):
+    user = models.ForeignKey('User', on_delete=models.CASCADE, related_name='diary')
     emoji = models.CharField(max_length=15)
     mood_temperature = models.IntegerField(default=25)
     date = models.DateField()
     contents = models.TextField()
     background_color = models.CharField(max_length=15)
     private = models.BooleanField(default=False)
-    image = models.ImageField(null=True)
+    image = models.URLField(blank=True, null=True)
 
     def __str__(self):
         return '{} 일기'.format(self.user.name)
 
 
-class Likes(models.Model):
+class Likes(BaseModel):
     # 좋아요를 누른 유저
-    click_user = models.ForeignKey('User', on_delete=models.CASCADE, verbose_name='user info')
-    todo = models.ManyToManyField(Todo)
-    diary = models.ManyToManyField(Diary)
+    click_user = models.ForeignKey('User', on_delete=models.CASCADE, related_name='user')
+    todo = models.ForeignKey('Todo', on_delete=models.CASCADE, related_name='todo')
+    diary = models.ForeignKey('Diary', on_delete=models.CASCADE, related_name='diary')
 
     def __str__(self):
         return '{}가 {}의 글을 좋아합니다.'.format(self.click_user.name, self.todo.user.name)
 
 
-class Follows(models.Model):
+class Follows(BaseModel):
     # 팔로우를 건 유저
     click_user = models.ManyToManyField(User, related_name='click_user')
     # 팔로우 타겟
