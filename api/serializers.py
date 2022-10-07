e2f304dea97cec7fbc1f88b61ca146029e8dbdea
suhhyun <2835223@ewhain.net>
2022-10-07 17:52:16 +0900

    Add serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,31 +1,35 @@
 from rest_framework import serializers
-from api.models import TodoList
+from django.contrib.auth.models import User
+from api.models import *
 
 
-class TodoListSerializer(serializers.Serializer):
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    user = UserSerializer
+
+    class Meta:
+        model = Profile
+        fields = ['user', 'introduction', 'search_yn', 'open_yn',
+                  'start_sunday_yn', 'order_desc_yn', 'input_top_yn', 'check_likes_yn']
+
+
+class TodoGroupSerializer(serializers.ModelSerializer):
+    user = UserSerializer
+
+    class Meta:
+        model = TodoGroup
+        fields = ['user', 'group', 'opened', 'color']
+
+
+class TodoListSerializer(serializers.ModelSerializer):
+    user = UserSerializer
+    group = TodoGroupSerializer
+
     class Meta:
         model = TodoList
-        fields = ['user', 'group', 'start_date', 'end_date', 'repeated_day', 'alarm_time', 'todo', 'image', 'status']
-
-    def create(self, validated_data):
-        """
-        Create and return a new `Snippet` instance, given the validated data.
-        """
-        return TodoList.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        """
-        Update and return an existing `Snippet` instance, given the validated data.
-        """
-        instance.user = validated_data.get('user', instance.user)
-        instance.group = validated_data.get('group', instance.group)
-        instance.start_date = validated_data.get('start_date', instance.start_date)
-        instance.end_date = validated_data.get('end_date', instance.end_date)
-        instance.repeated_day = validated_data.get('repeated_day', instance.repeated_day)
-        instance.alarm_time = validated_data.get('alarm_time', instance.alarm_time)
-        instance.todo = validated_data.get('todo', instance.todo)
-        instance.image = validated_data.get('image', instance.image)
-        instance.status = validated_data.get('status', instance.status)
-
-        instance.save()
-        return instance
+        fields = ['user', 'group', 'start_date', 'end_date', 'repeated_day', 'alarm_time', 'todo', 'status']
