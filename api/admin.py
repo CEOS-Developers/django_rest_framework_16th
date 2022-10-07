2e31026ae1e93eb345c4c5096a8d5d935836c235
Jeong-Hyeon-Lee <hyeon00@ewhain.net>
2022-10-07 22:08:34 +0900

    add user manager (fix createsuperuser error)

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from api.models import *
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Follower)
+admin.site.register(Category)
+admin.site.register(Todo)
+admin.site.register(Comment)
