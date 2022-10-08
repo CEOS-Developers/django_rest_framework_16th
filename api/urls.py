ebbc65e7d01d823fed31409b75062bcf9219b179
Jeong-Hyeon-Lee <hyeon00@ewhain.net>
2022-10-08 20:51:28 +0900

    create put, delete api for todo

--- api/urls.py
+++ api/urls.py
@@ -4,8 +4,6 @@ from . import views
 urlpatterns = [
 
     #Todo
-    path('todo/', views.todo_list_all, name="todo_all"),
-    #'todo/': 모든 user들의 todolist가 다 보이기 때문에 실제로는 쓸 일이 없을 것 같지만.. 모든 데이터 가져오기 연습을 위해 만듦
-
-    path('todo/<int:pk>', views.todo_user, name="todo_user")
+    path('todo/', views.todo_list, name="todo_list"),
+    path('todo/<int:pk>', views.todo_item, name="todo_item"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -10,19 +9,10 @@ from api.serializers import *
 # Create your views here.
 @csrf_exempt
 @api_view(['GET', 'POST'])
-def todo_list_all(request):
+def todo_list(request):
     if request.method == 'GET':
         todo_items = Todo.objects.all()
         serializer = TodoSerializer(todo_items, many=True)
-        return Response(serializer.data)
-
-
-@csrf_exempt
-@api_view(['GET','POST'])
-def todo_user(request, pk):
-    if request.method == 'GET':
-        todo_item = Todo.objects.filter(user=pk)
-        serializer = TodoSerializer(todo_item, many=True)
         return Response(serializer.data)
 
     elif request.method == 'POST':
@@ -31,3 +21,27 @@ def todo_user(request, pk):
             serializer.save()
             return Response(serializer.data, status=201)
         return Response(serializer.errors, status=400)
+
+
+@csrf_exempt
+@api_view(['GET', 'POST'])
+def todo_item(request, pk):
+
+    if request.method == 'GET':
+        todo = Todo.objects.filter(id=pk)
+        serializer = TodoSerializer(todo, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        todo_instance = Todo.objects.filter(id=pk)
+        serializer = TodoSerializer(instance=todo_instance, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        todo = Todo.objects.filter(id=pk)
+        todo.delete()
+        return Response(status=201)
+
