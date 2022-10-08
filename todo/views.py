a1bac2ecf394ecdc269f2494504c5eb83029cc94
aqswa <69039161+aqswa@users.noreply.github.com>
2022-10-08 18:07:38 +0900

    Feat: add GoalSerializer

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from .models import Goal, Todo
+from .serializers import GoalSerializer
 
-# Create your views here.
+
+@csrf_exempt
+def goal_list(request):
+    if request.method == 'GET':
+        goals = Goal.objects.all()
+        serializer = GoalSerializer(goals)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = GoalSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
