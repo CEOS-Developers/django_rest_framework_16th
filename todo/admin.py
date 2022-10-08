c969f7617bc8d7463887b287a85d0bee2f7484f6
aqswa <69039161+aqswa@users.noreply.github.com>
2022-10-08 17:59:04 +0900

    Feat: admin register

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import *
+
 
 # Register your models here.
+
+@admin.register(Goal)
+class GoalAdmin(admin.ModelAdmin):
+    list_display = ['title', 'user', 'privacy', 'color']
+
+
+@admin.register(Todo)
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ['goal', 'title', 'is_done']
