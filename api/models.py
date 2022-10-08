762a243e884cdc095f84deba2ce2225bd682e7db
yjoonjang <yjoonjang34@gmail.com>
2022-10-08 16:41:28 +0900

    REST api 세팅 및 models 수정

--- api/models.py
+++ api/models.py
@@ -2,11 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class User(models.Model):
-    id = models.BigIntegerField()   # key
-    username = models.TextField(max_length=30)
-    password = models.TextField(max_length=32)
+    # id = models.BigIntegerField()   # key
+    username = models.CharField(max_length=30)
+    password = models.CharField(max_length=32)
     email = models.TextField()
-    profile_image = models.imageField()
+    profile_image = models.ImageField()
     can_search_by_email = models.TextField()
     is_shown = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)  # 해당 레코드 생성시 현재 시간 자동저장
@@ -14,21 +14,25 @@ class User(models.Model):
     deleted_at = models.DateTimeField()
 
 class Todo(models.Model):
-    id = models.BigIntegerField()
-    user_id = models.BigIntegerField()
-    is_done = models.BooleanField()
-    is_stored = models.BooleanField()
+    # id = models.BigIntegerField()
+    # user = models.ForeignKey(User, related_name='todo', on_delete=models.cascade)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # user_id = models.BigIntegerField()
+    is_done = models.BooleanField(default=False)
+    is_stored = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     deleted_at = models.DateTimeField()
 
 class Diary(models.Model):
-    id = models.BigIntegerField()
-    user_id = models.BigIntegerField()
+    # id = models.BigIntegerField()
+    # user = models.ForeignKey(User, related_name='todo', on_delete=models.cascade)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # user_id = models.BigIntegerField()
     content = models.TextField()
     bg_color = models.TextField()
     feeling_temp = models.IntegerField()
-    emoji = models.CharField()
+    emoji = models.CharField(max_length=10)
     is_secret = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
