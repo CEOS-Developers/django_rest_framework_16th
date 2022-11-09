81854b4965632ae6c2a95e4ad5a59c0f99a7c56b
squirMM <jsm6616@naver.com>
2022-11-09 19:49:10 +0900

    :rocket: feat(fiter): set filter config

--- django_rest_framework_16th/settings/base.py
+++ django_rest_framework_16th/settings/base.py
@@ -46,7 +46,15 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api',
     'rest_framework',
+    'django_filters',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+        ...
+    ),
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
