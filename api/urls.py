f58e1596af8b775df8cea2ffbb5930260b2907c4
chaeshee0908 <chaeshee0908@naver.com>
2022-11-10 02:42:22 +0900

    RestfulAPI를 위한 url 수정

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("todo/", TodoListsAPI.as_view()),
-    path("todo/<int:pk>/", TodoListAPI.as_view()),
+    path("todos/", TodoListsAPI.as_view()),
+    path("todos/<int:pk>/", TodoListAPI.as_view()),
 ]
