3e03f0ce2da55e7b62b28a421b8c7f511bc7f5ff
suabae <bsa0322@ewhain.net>
2022-11-11 05:23:49 +0900

    [FIX] viewset

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,15 @@
-from django.urls import path, include
-from .views import TodoList, TodoDetail
+# from django.urls import path, include
+# from .views import TodoList, TodoDetail
+#
+# app_name = 'api'
+#
+# urlpatterns = [path('todos/', TodoList.as_view()),
+#                path('todos/<int:id>', TodoDetail.as_view())]
 
-app_name = 'api'
+from rest_framework import routers
+from .views import TodoViewSet
 
-urlpatterns = [path('todos/', TodoList.as_view()),
-               path('todos/<int:id>', TodoDetail.as_view())]
+router = routers.DefaultRouter()
+router.register(r'todos', TodoViewSet)  # register()함으로써 두 개의 url 생성
+
+urlpatterns = router.urls
--- api/views.py
+++ api/views.py
@@ -1,51 +1,60 @@
 # views.py
 
-from rest_framework.views import APIView
-from rest_framework.generics import get_object_or_404
+# from rest_framework.views import APIView
+# from rest_framework.generics import get_object_or_404
+# from api.models import ToDo
+# from api.serializers import ToDoSerializer
+# from django.http import JsonResponse
+# from rest_framework.parsers import JSONParser
+# from rest_framework.response import Response
+#
+#
+# class TodoList(APIView):
+#
+#     def get(self, request):  # 모든 데이터 얻기
+#         todo = ToDo.objects.all()
+#         serializer = ToDoSerializer(todo, many=True)
+#         return JsonResponse(serializer.data, status=200, safe=False)
+#
+#     def post(self, request):  # 새로운 데이터 등록
+#         data = JSONParser().parse(request)
+#         serializer = ToDoSerializer(data=data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return JsonResponse(serializer.data, status=201)
+#         return JsonResponse(serializer.errors, status=400)
+#
+#
+# class TodoDetail(APIView):
+#
+#     def get_object(self, id):
+#         todo = get_object_or_404(ToDo, id=id)
+#         return todo
+#
+#     def get(self, request, id):  # 특정 데이터 얻기
+#         todo = self.get_object(id)
+#         serializer = ToDoSerializer(todo)
+#         return JsonResponse(serializer.data, status=200, safe=False)
+#
+#     def delete(self, request, id):  # 특정 데이터 삭제
+#         todo = self.get_object(id)
+#         todo.delete()
+#         return Response(status=204)
+#
+#     def patch(self, request, id):  # 특정 데이터 업데이트
+#         todo = self.get_object(id)
+#         data = JSONParser().parse(request)
+#         serializer = ToDoSerializer(todo, data=data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return JsonResponse(serializer.data, status=200)
+#         return JsonResponse(serializer.errors, status=400)
+
+from rest_framework.viewsets import ModelViewSet
 from api.models import ToDo
 from api.serializers import ToDoSerializer
-from django.http import JsonResponse
-from rest_framework.parsers import JSONParser
-from rest_framework.response import Response
 
 
-class TodoList(APIView):
-
-    def get(self, request):  # 모든 데이터 얻기
-        todo = ToDo.objects.all()
-        serializer = ToDoSerializer(todo, many=True)
-        return JsonResponse(serializer.data, status=200, safe=False)
-
-    def post(self, request):  # 새로운 데이터 등록
-        data = JSONParser().parse(request)
-        serializer = ToDoSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
-
-
-class TodoDetail(APIView):
-
-    def get_object(self, id):
-        todo = get_object_or_404(ToDo, id=id)
-        return todo
-
-    def get(self, request, id):  # 특정 데이터 얻기
-        todo = self.get_object(id)
-        serializer = ToDoSerializer(todo)
-        return JsonResponse(serializer.data, status=200, safe=False)
-
-    def delete(self, request, id):  # 특정 데이터 삭제
-        todo = self.get_object(id)
-        todo.delete()
-        return Response(status=204)
-
-    def patch(self, request, id):  # 특정 데이터 업데이트
-        todo = self.get_object(id)
-        data = JSONParser().parse(request)
-        serializer = ToDoSerializer(todo, data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=200)
-        return JsonResponse(serializer.errors, status=400)
+class TodoViewSet(ModelViewSet):
+    serializer_class = ToDoSerializer
+    queryset = ToDo.objects.all()
