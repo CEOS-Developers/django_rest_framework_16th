753a2dffb227ce6f2435f4189f613fd2f48fb528
Jeong-Hyeon-Lee <hyeon00@ewhain.net>
2022-11-12 16:58:16 +0900

    refactor to Class-Based View

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
-from . import views
+#from . import views
+from api.views import TodoList, TodoItem
 
 urlpatterns = [
-
     #Todo
-    path('todo/', views.todo_list, name="todo_list"),
-    path('todo/<int:pk>', views.todo_item, name="todo_item"),
+    path('todo/', TodoList.as_view(), name="todo-list"),
+    path('todo/<int:pk>/', TodoItem.as_view(), name="todo-item"),
 ]
+
+"""
+Urls for Fuction-Based View
+#Todo
+path('todo/', views.todo_list, name="todo_list"),
+path('todo/<int:pk>', views.todo_item, name="todo_item"),
+"""
--- api/views.py
+++ api/views.py
@@ -4,9 +4,41 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from api.models import *
 from api.serializers import *
-
+from rest_framework.views import APIView
 
 # Create your views here.
+class TodoList(APIView):
+    def get(self, request, format=None):
+        todo_items = Todo.objects.all()
+        serializer = TodoSerializer(todo_items, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = TodoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+
+class TodoItem(APIView):
+    def get(self, request, pk, format=None):
+        todo = Todo.objects.filter(id=pk)
+        serializer = TodoSerializer(todo, many=True)
+        return Response(serializer.data)
+    def put(self, request, pk, format=None):
+        todo_instance = Todo.objects.get(id=pk)
+        serializer = TodoSerializer(instance=todo_instance, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+    def delete(self, request, pk, format=None):
+        todo = Todo.objects.filter(id=pk)
+        todo.delete()
+        return Response(status=201)
+
+"""
+Function-Based View
 @csrf_exempt
 @api_view(['GET', 'POST'])
 def todo_list(request):
@@ -45,3 +77,5 @@ def todo_item(request, pk):
         todo.delete()
         return Response(status=201)
 
+
+"""
