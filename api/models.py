284c00971bdb236f854957fdb96fedc08feab624
bokdol11859 <2019147551@yonsei.ac.kr>
2022-11-12 21:27:44 +0900

    implement filterset

--- api/models.py
+++ api/models.py
@@ -2,11 +2,10 @@ from django.db import models
 
 # Create your models here.
 
-# class TimeStamp(models.Model):
-#     # id = models.AutoField(primary_key=True)
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
-#     deleted_flag = models.BooleanField(default=False)
+class TimeStamp(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_flag = models.BooleanField(default=False)
 
 class User(models.Model):
     id = models.AutoField(primary_key=True)
--- api/views.py
+++ api/views.py
@@ -1,58 +1,41 @@
 # Create your views here.
 import datetime
 
-from django.shortcuts import get_object_or_404
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework import status, viewsets
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework.views import APIView
+import django_filters
+from django_filters import filters
+from django_filters.rest_framework import FilterSet, DjangoFilterBackend
+from rest_framework import viewsets
 
 from api.serializers import *
+
+
+class TodoClassFilter(FilterSet):
+    opened_todo_classes = filters.NumberFilter(field_name='is_open', lookup_expr='exact')
+    searched_todo_class = filters.CharFilter(field_name='class_name', lookup_expr='contains')
+
+    class Meta:
+        model = TodoClass
+        fields = ['is_open', 'class_name']
 
 
 class TodoClassesViewSet(viewsets.ModelViewSet):
     queryset = TodoClass.objects.all()
     serializer_class = TodoClassSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = TodoClassFilter
+
+
+class TodoFilter(FilterSet):
+    finished_todos = filters.BooleanFilter(field_name='is_finished', lookup_expr='exact')
+    todo_title = filters.CharFilter(field_name='title', lookup_expr='contains')
+
+    class Meta:
+        model = Todo
+        fields = ['is_finished', 'title']
 
 
 class TodoViewSet(viewsets.ModelViewSet):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
-
-#
-# class TodosAPI(APIView):
-#     def get(self, request):
-#         todos = Todo.objects.filter(deleted_at=None)  # filter out soft-deleted todos
-#         if len(todos) == 0:
-#             return Response("Todo does not exist", status=status.HTTP_404_NOT_FOUND)
-#         serializer = TodosSerializer(todos, many=True)
-#         return Response(serializer.data, status=status.HTTP_200_OK)
-#
-#     def post(self, request):
-#         serializer = TodosSerializer(data=request.data)  # create new todo
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#
-#
-# class TodoAPI(APIView):
-#     def get(self, request, id=id):
-#         todo = get_object_or_404(Todo, id=id)
-#         if (todo.deleted_flag):  # check if the todo had been soft-deleted
-#             return Response("Todo does not exist", status=status.HTTP_404_NOT_FOUND)
-#         serializer = TodoSerializer(todo)
-#         return Response(serializer.data, status=status.HTTP_200_OK)
-#
-#     def patch(self, request, id):
-#         todo = get_object_or_404(Todo, id=id)
-#         if (request.data != {}):
-#             todo.title = request.data['title']
-#             todo.image = request.data['image']
-#             todo.is_finished = request.data['is_finished']
-#             todo.is_default = request.data['is_default']
-#         else:
-#             todo.deleted_flag = True  # soft-delete todo
-#         todo.save()
-#         serializer = TodosSerializer(todo)
-#         return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = TodoFilter
