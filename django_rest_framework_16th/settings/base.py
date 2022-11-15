a2a8196b77cce1dff246a65489fdc8ee2ab4470b
jianny-lee <jianny.lee96@gmail.com>
2022-11-15 19:15:18 +0900

    edit base.py

--- django_rest_framework_16th/settings/base.py
+++ django_rest_framework_16th/settings/base.py
@@ -42,7 +42,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api',
     'rest_framework',
+    'django_filters',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
