633a2f3e0ba38b5f3d211500785f1212e059d43c
squirMM <jsm6616@naver.com>
2022-11-16 18:48:27 +0900

    :rocket: feat(JWT): add urls

--- django_rest_framework_16th/urls.py
+++ django_rest_framework_16th/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_jwt.views import obtain_jwt_token, verify_jwt_token, refresh_jwt_token
 import api
 
 urlpatterns = [
     path('', admin.site.urls),
+    path('api/token/', obtain_jwt_token),
+    path('api/token/verify/', verify_jwt_token),
+    path('api/token/refresh/', refresh_jwt_token),
     path('api/', include('api.urls')),
 ]
