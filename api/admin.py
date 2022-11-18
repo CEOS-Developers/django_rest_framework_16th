48becde1a2c3844ac3ffe8870b524a47b8dc09e2
suhhyun <2835223@ewhain.net>
2022-11-19 07:08:44 +0900

    Add form

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,122 @@
 from django.contrib import admin
+from django import forms
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+
 from .models import *
 
-# Register your models here.
-admin.site.register(Profile)
+
+class UserCreationForm(forms.ModelForm):
+    id = forms.CharField(label='id', required=True, widget=forms.TextInput)
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('id',)
+
+    def save(self, commit=True):
+        user = super(UserCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
+
+
+class UserChangeForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = User
+        fields = ('id', 'password')
+
+    def clean_password(self):
+        return self.initial["password"]
+
+
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    list_display = ('id', 'is_admin')
+    list_filter = ('is_admin',)
+    fieldsets = (
+        (None, {'fields': ('id', 'password')}),
+        ('Permissions', {'fields': ('is_admin',)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('id', 'password1')}
+         ),
+    )
+    search_fields = ('id',)
+    ordering = ('id',)
+    filter_horizontal = ()
+
+
+# class UserCreationForm(forms.ModelForm):
+#     email = forms.EmailField(label='email', required=True, widget=forms.EmailInput)
+#     #nickname = forms.CharField(label='nickname', required=False, widget=forms.TextInput)
+#     password = forms.CharField(label='Password', widget=forms.PasswordInput)
+#     # password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+#
+#     class Meta:
+#         model = User
+#         fields = ('email',)
+
+    # def clean_password2(self):
+    #     password1 = self.cleaned_data.get("password1")
+    #     password2 = self.cleaned_data.get("password2")
+    #
+    #     if password1 and password2 and password1 != password2:
+    #         raise forms.ValidationError("Passwords don't match")
+    #     return password2
+
+    # def save(self, commit=True):
+    #     user = super(UserCreationForm, self).save(commit=False)
+    #     user.set_password(self.cleaned_data["password"])
+    #     if commit:
+    #         user.save()
+    #     return user
+# class UserChangeForm(forms.ModelForm):
+#     password = ReadOnlyPasswordHashField()
+#
+#     class Meta:
+#         model = User
+#         fields = ('email', 'password', 'nickname', 'is_active', 'is_admin')
+#
+#     def clean_password(self):
+#         return self.initial["password"]
+#
+#
+# class UserAdmin(BaseUserAdmin):
+#     form = UserChangeForm
+#     add_form = UserCreationForm
+#
+#     list_display = ('email', 'nickname', 'is_admin')
+#     list_filter = ('is_admin',)
+#     fieldsets = (
+#         (None, {'fields': ('email', 'password')}),
+#         ('Personal info', {'fields': ('nickname',)}),
+#         ('Permissions', {'fields': ('is_admin',)}),
+#     )
+#
+#     add_fieldsets = (
+#         (None, {
+#             'classes': ('wide',),
+#             'fields': ('email', 'nickname', 'password1', 'password2')}
+#          ),
+#     )
+#     search_fields = ('email',)
+#     ordering = ('email',)
+#     filter_horizontal = ()
+
+
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
+# admin.site.register(Profile)
 admin.site.register(TodoGroup)
 admin.site.register(Todo)
 admin.site.register(TodoLikes)
