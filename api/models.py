17bcd11029538d8b40e635f2266e2651335961b4
Jeong-Hyeon-Lee <hyeon00@ewhain.net>
2022-11-19 03:09:24 +0900

    refactor user model

--- api/models.py
+++ api/models.py
@@ -19,21 +19,25 @@ class BaseModel(models.Model):
 class UserManager(BaseUserManager):
     use_in_migrations = True
 
-    def _create_user(self, email, password, **extra_fields):
+    def _create_user(self, id, nickname, email, password, **extra_fields):
+        if not id:
+            raise ValueError('Users require an id field')
+        if not nickname:
+            raise ValueError('Users require a nickname field')
         if not email:
             raise ValueError('Users require an email field')
         email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+        user = self.model(id=id, nickname=nickname, email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_user(self, email, password=None, **extra_fields):
+    def create_user(self, id, nickname, email, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', False)
         extra_fields.setdefault('is_superuser', False)
-        return self._create_user(email, password, **extra_fields)
+        return self._create_user(id, nickname, email, password, **extra_fields)
 
-    def create_superuser(self, email, password, **extra_fields):
+    def create_superuser(self, id, nickname, email, password, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
@@ -42,7 +46,7 @@ class UserManager(BaseUserManager):
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser=True.')
 
-        return self._create_user(email, password, **extra_fields)
+        return self._create_user(id, nickname, email, password, **extra_fields)
 
 
 class User(AbstractBaseUser, BaseModel):
