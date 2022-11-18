96a4d4b05837a198614698accd2c3ab8fd955fcc
suhhyun <2835223@ewhain.net>
2022-11-19 07:10:31 +0900

    Add UserSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from api.models import *
@@ -6,17 +7,17 @@ from api.models import *
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
+        fields = ['user_id']
 
 
-class ProfileSerializer(serializers.ModelSerializer):
-    user = UserSerializer
-
-    class Meta:
-        model = Profile
-        fields = ['user', 'introduction', 'search_yn', 'open_yn',
-                  'start_sunday_yn', 'order_desc_yn', 'input_top_yn', 'check_likes_yn']
-
+# class ProfileSerializer(serializers.ModelSerializer):
+#     user = UserSerializer
+#
+#     class Meta:
+#         model = Profile
+#         fields = ['user', 'introduction', 'search_yn', 'open_yn',
+#                   'start_sunday_yn', 'order_desc_yn', 'input_top_yn', 'check_likes_yn']
+#
 
 class TodoGroupSerializer(serializers.ModelSerializer):
     user = UserSerializer
