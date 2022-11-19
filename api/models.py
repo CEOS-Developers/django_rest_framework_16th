9541539674d1d5c87566a64726ce692e7ec71050
suhhyun <2835223@ewhain.net>
2022-11-19 23:32:20 +0900

    Add Custom User Model

--- api/models.py
+++ api/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.db import models
 from django.contrib.auth.models import (
     BaseUserManager, AbstractBaseUser
 )
@@ -57,78 +56,57 @@ class UserManager(BaseUserManager):
         return user
 
 
-class User(AbstractBaseUser):
-    id = models.CharField(max_length=254, primary_key=True, unique=True)
+class MyUser(AbstractBaseUser):
+    email = models.EmailField(max_length=255, unique=True)
+    nickname = models.CharField(max_length=20, default='me')
+    image = models.TextField(null=True)
+    search_yn = models.BooleanField(default=True)
+    open_yn = models.BooleanField(default=True)
+    start_sunday_yn = models.BooleanField(default=False)
+    order_desc_yn = models.BooleanField(default=True)
+    input_top_yn = models.BooleanField(default=False)
+    check_likes_yn = models.BooleanField(default=True)
 
     is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
 
     objects = UserManager()
 
-    USERNAME_FIELD = 'id'
+    USERNAME_FIELD = 'email'
 
     class Meta:
-        db_table = "User"
+        db_table = "MyUser"
 
     def __str__(self):
-        return self.id
+        return self.email
 
+    def get_nickname(self):
+        return self.nickname
 
-# class UserManager(BaseUserManager):
-#     def create_user(self, email, password):
-#
-#         if not email:
-#             raise ValueError('Email cannot be null')
-#
-#         if not password:
-#             raise ValueError('Password cannot be null')
-#
-#         user = self.model(
-#             email=self.normalize_email(email),
-#         )
-#
-#         user.set_password(password)
-#         user.save(using=self._db)
-#         return user
-#
-#     def create_superuser(self, email, password):
-#
-#         user = self.create_user(
-#             email,
-#             password=password,
-#         )
-#         user.is_admin = True
-#         user.save(using=self._db)
-#         return user
-#
-#
-# class User(AbstractBaseUser):
-#     email = models.EmailField(max_length=255, unique=True)
-#     nickname = models.CharField(max_length=20, default='me')
-#     image = models.TextField(null=True)
-#     search_yn = models.BooleanField(default=True)
-#     open_yn = models.BooleanField(default=True)
-#     start_sunday_yn = models.BooleanField(default=False)
-#     order_desc_yn = models.BooleanField(default=True)
-#     input_top_yn = models.BooleanField(default=False)
-#     check_likes_yn = models.BooleanField(default=True)
-#
-#     is_active = models.BooleanField(default=True)
-#     is_admin = models.BooleanField(default=False)
-#
-#     objects = UserManager()
-#
-#     USERNAME_FIELD = 'email'
-#
-#     class Meta:
-#         db_table = "User"
-#
-#     def __str__(self):
-#         return self.email
-#
-#     def get_nickname(self):
-#         return self.nickname
+    def get_full_name(self):
+        pass
 
+    def get_short_name(self):
+        pass
+
+    @property
+    def is_superuser(self):
+        return self.is_admin
+
+    @property
+    def is_staff(self):
+        return self.is_admin
+
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        return self.is_admin
+
+    @is_staff.setter
+    def is_staff(self, value):
+        self._is_staff = value
 
 # class Profile(BaseModel):
 #     user = models.OneToOneField(User, db_column='user', on_delete=models.CASCADE)
@@ -149,7 +127,7 @@ class User(AbstractBaseUser):
 
 
 class TodoGroup(BaseModel):
-    user = models.ForeignKey(User, related_name='group', db_column='user', on_delete=models.CASCADE)
+    user = models.ForeignKey(MyUser, related_name='group', db_column='user', on_delete=models.CASCADE)
     group = models.TextField()
     opened = models.TextField(default='all')
     color = models.CharField(max_length=10, default='#000000')
@@ -162,7 +140,7 @@ class TodoGroup(BaseModel):
 
 
 class Todo(BaseModel):
-    user = models.ForeignKey(User, db_column='user', on_delete=models.CASCADE)
+    user = models.ForeignKey(MyUser, db_column='user', on_delete=models.CASCADE)
     group = models.ForeignKey(TodoGroup, related_name='todo', db_column='group', on_delete=models.CASCADE)
     start_date = models.DateField(default=datetime.date.today)
     end_date = models.DateField(default=datetime.date.today)
@@ -181,7 +159,7 @@ class Todo(BaseModel):
 
 class TodoLikes(BaseModel):
     todo_id = models.ForeignKey(Todo, related_name='todo_likes', db_column='todo_id', on_delete=models.CASCADE)
-    user = models.ForeignKey(User, related_name='todo_likes', db_column='user', on_delete=models.CASCADE)
+    user = models.ForeignKey(MyUser, related_name='todo_likes', db_column='user', on_delete=models.CASCADE)
     emoticon = models.ImageField()
 
     class Meta:
@@ -192,7 +170,7 @@ class TodoLikes(BaseModel):
 
 
 class Diary(BaseModel):
-    user = models.ForeignKey(User, related_name='diary', db_column='user', on_delete=models.CASCADE)
+    user = models.ForeignKey(MyUser, related_name='diary', db_column='user', on_delete=models.CASCADE)
     emoji = models.CharField(max_length=20, null=True)
     contents = models.TextField(max_length=500)
     bg_color = models.CharField(max_length=10, default='#FFFFFF')
@@ -209,7 +187,7 @@ class Diary(BaseModel):
 
 class DiaryLikes(BaseModel):
     diary_id = models.ForeignKey(Diary, related_name='diary_likes', db_column='diary_id', on_delete=models.CASCADE)
-    user = models.ForeignKey(User, related_name='diary_likes', db_column='user', on_delete=models.CASCADE)
+    user = models.ForeignKey(MyUser, related_name='diary_likes', db_column='user', on_delete=models.CASCADE)
     emoticon = models.ImageField()
 
     class Meta:
@@ -220,8 +198,8 @@ class DiaryLikes(BaseModel):
 
 
 class Relation(BaseModel):
-    follower = models.ForeignKey(User, related_name='follower', db_column='follower', on_delete=models.CASCADE)
-    followee = models.ForeignKey(User, related_name='followee', db_column='followee', on_delete=models.CASCADE)
+    follower = models.ForeignKey(MyUser, related_name='follower', db_column='follower', on_delete=models.CASCADE)
+    followee = models.ForeignKey(MyUser, related_name='followee', db_column='followee', on_delete=models.CASCADE)
 
     class Meta:
         db_table = "Relation"
