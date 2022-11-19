ce6518af8a464b63610ef430f3b6395f10ff14e9
Jeong-Hyeon-Lee <hyeon00@ewhain.net>
2022-11-19 23:16:48 +0900

    fix admin user error

--- api/models.py
+++ api/models.py
@@ -33,20 +33,18 @@ class UserManager(BaseUserManager):
         return user
 
     def create_user(self, id, nickname, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', False)
-        extra_fields.setdefault('is_superuser', False)
         return self._create_user(id, nickname, email, password, **extra_fields)
 
     def create_superuser(self, id, nickname, email, password, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
-
-        return self._create_user(id, nickname, email, password, **extra_fields)
+        user = self.create_user(
+            id=id,
+            nickname=nickname,
+            email=email,
+            password=password,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
 
 
 class User(AbstractBaseUser, BaseModel):
@@ -54,9 +52,21 @@ class User(AbstractBaseUser, BaseModel):
     nickname = models.CharField(max_length=20)
     email = models.EmailField(max_length=255, unique=True)
 
-    is_superuser = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.fullname
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
 
     objects = UserManager()
 
