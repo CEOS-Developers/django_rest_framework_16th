f0e9aa4ab3901235591550b5d534de3711225ab0
suhhyun <2835223@ewhain.net>
2022-11-19 17:26:44 +0900

    Add JoinView

--- api/urls.py
+++ api/urls.py
@@ -12,14 +12,14 @@ urlpatterns = format_suffix_patterns(urlpatterns)
 
 from django.urls import path
 from rest_framework import routers
-from .views import TodoViewSet, LoginView  # , JoinView
+from .views import TodoViewSet, LoginView, JoinView
 from rest_framework_simplejwt.views import TokenRefreshView
 
 router = routers.DefaultRouter()
 router.register(r'todo', TodoViewSet)
 
 urlpatterns = [
-    # path('join/', JoinView.as_view()),
+    path('join/', JoinView.as_view()),
     path('login/', LoginView.as_view()),
     path('login/refresh/', TokenRefreshView.as_view()),
 ]
