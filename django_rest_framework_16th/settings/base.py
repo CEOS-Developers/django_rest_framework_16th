46faff0d1a9becf166ee59e3a6b794dfa821643c
bokdol11859 <2019147551@yonsei.ac.kr>
2022-11-26 21:34:36 +0900

    build: allowed host fix

--- django_rest_framework_16th/settings/base.py
+++ django_rest_framework_16th/settings/base.py
@@ -30,7 +30,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ["http://3.38.166.164/"]
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
