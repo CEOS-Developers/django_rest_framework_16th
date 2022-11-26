ecd8ea54b726789d175a5b2e3a04c85b68ba56b3
Jeong-Hyeon-Lee <hyeon00@ewhain.net>
2022-11-27 03:02:51 +0900

    Update wsgi.py

--- django_rest_framework_16th/wsgi.py
+++ django_rest_framework_16th/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_rest_framework_16th.settings.prod')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_rest_framework_16th.settings')
 
 application = get_wsgi_application()
