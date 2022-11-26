fff2f2c5b04fd7d5b61f1f24bd4ef3045f848bc3
geniee44 <68368633+geniee44@users.noreply.github.com>
2022-11-26 22:09:45 +0900

    add some modules

--- django_rest_framework_16th/settings/base.py
+++ django_rest_framework_16th/settings/base.py
@@ -12,10 +12,10 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import environ
-# import pymysql
+import pymysql
 from datetime import timedelta
 
-# pymysql.install_as_MySQLdb()
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
